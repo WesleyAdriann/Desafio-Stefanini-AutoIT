93a03d22121d02478617584c29f477dffe53c661
Wesley Souza <wesleyadriann@gmail.com>
2019-03-20 14:35:58 -0300

    Cria o formulario da correção

--- src/components/correcaoAutomatica.js
+++ src/components/correcaoAutomatica.js
@@ -4,7 +4,23 @@ class CorrecaoAutomatica extends Component {
     render() {
         return (
             <div>
-                Correcao
+                <form>
+                    <div className="form-group">
+                        <label for="name">Nome</label>
+                        <input type="text" className="form-control" id="name" placeholder="Informe seu nome"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" className="form-control" id="email" placeholder="nome@exemplo.com"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label for="description">Descrição</label>
+                        <textarea  className="form-control" id="description" rows="3"/>
+                        
+                    </div>
+                </form>
             </div>
         );
     }
