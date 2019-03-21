ed543412fc203a446edc9c03f03166aff9371763
Wesley Souza <wesleyadriann@gmail.com>
2019-03-21 00:21:56 -0300

    Passa as funçõees de rest para analise de texto

--- src/components/AnaliseTexto.js
+++ src/components/AnaliseTexto.js
@@ -1,16 +1,115 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class AnaliseTexto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            info : this.props.info
+            palavrasChave : '',
+            lingua: '',
+            sentimento: '',
+            flag: false,
+        }
+    }
+    
+    componentDidMount = () => {
+        if(this.state.flag === false) {
+            this.handleLingua(this.props.descricao);
+            this.handlePalavras(this.props.descricao);
+            this.handleSentimento(this.props.descricao);
+        }
+    }
+
+    handleLingua = descricao => {
+        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        axios({
+            method: 'post',
+            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/languages',
+            headers : {
+                'Ocp-Apim-Subscription-Key' : key,
+            },
+            data : {
+                "documents" : [
+                    {
+                        "id" : "1",
+                        "text" : descricao,
+                    }
+                ]
+            },
+
+        }).then (response => {
+            this.setState({
+                lingua : response.data.documents[0].detectedLanguages,
+                flag: true,
+            })
+        })
+        
+    }
+
+    handleSentimento = descricao => {
+        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        axios({
+            method: 'post',
+            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+            headers : {
+                'Ocp-Apim-Subscription-Key' : key,
+            },
+            data : {
+                "documents": [
+                    {
+                        // "language" : responseIdioma.iso6391Name,
+                        "id" : "1",
+                        "text" : descricao, 
+                    }
+                ]
+            }
+        }).then (response => {
+            console.log(response);
+            this.setState({
+                sentimento : response.data.documents[0].score,
+                flag: true,
+            })
+        })
+    }
+
+    handlePalavras = descricao => {
+        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        axios({
+            method: 'post',
+            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+            headers : {
+                'Ocp-Apim-Subscription-Key' : key,
+            },
+            data : {
+                "documents" : [
+                    {
+                        // "language" : responseIdioma.iso6391Name,
+                        "id" : "1",
+                        "text" : descricao, 
+                    }
+                ]
+            }, 
+        }).then (response => {
+            this.setState({
+                palavrasChave : response.data.documents[0].keyPhrases,
+                flag: true,
+            })
+        })
+    }
+
+    reRender = () =>{
+        if(this.state.flag) {
+            this.setState({flag: false});
         }
     }
     render () {
+        window.setTimeout(this.reRender, 2000);
         return (
             <div>
-                {console.log(this.state.info.responseSentimento)}
+                <strong style={{fontSize: '14px'}}>Sentimento</strong> <br/>
+                <div className="progress">
+                <div class="progress-bar" role="progressbar" style={{width: `${this.state.sentimento*100}%`}}>{parseInt(this.state.sentimento*100)}%</div>
+                </div>
             </div>
         )
     }
--- src/components/PainelEmocao.js
+++ src/components/PainelEmocao.js
@@ -6,99 +6,28 @@ class PainelEmocao extends Component {
     constructor(props) {
         super(props);
         this.data = props.data;
-        this.state = {
-            idioma : [],
-        }
-        this.handleDescricao = this.handleDescricao.bind(this);
     }
 
-    async handleDescricao (descricao) {
-        const key = "9a1b21c339c1401e8c486ca264d333f5";
-        
-        let responseIdioma = await axios({
-            method: 'post',
-            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/languages',
-            headers : {
-                'Ocp-Apim-Subscription-Key' : key,
-            },
-            data : {
-                "documents" : [
-                    {
-                        "id" : "1",
-                        "text" : descricao,
-                    }
-                ]
-            },
-
-        })
-        responseIdioma = responseIdioma.data.documents[0].detectedLanguages;
-        let responseFrases = await axios({
-            method: 'post',
-            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
-            headers : {
-                'Ocp-Apim-Subscription-Key' : key,
-            },
-            data : {
-                "documents" : [
-                    {
-                        "language" : responseIdioma.iso6391Name,
-                        "id" : "1",
-                        "text" : descricao, 
-                    }
-                ]
-            },
-        })
-        responseFrases = responseFrases.data.documents[0].keyPhrases;
-
-        let responseSentimento = await axios({
-            method: 'post',
-            url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
-            headers : {
-                'Ocp-Apim-Subscription-Key' : key,
-            },
-            data : {
-                "documents": [
-                    {
-                        "language" : responseIdioma.iso6391Name,
-                        "id" : "1",
-                        "text" : descricao, 
-                    }
-                ]
-            }
-        })
-        responseSentimento = responseSentimento.data.documents[0].score;
-        
-        const completeInfos = {responseIdioma, responseFrases, responseSentimento};
-        console.log(completeInfos);
-        return (completeInfos);
-            
-    }
-
-
-
-    render () {   
+    render () { 
         return (
             <div className="row">
                 {
                     this.data.map(info => { 
-                        let newInfos = this.handleDescricao(info.userDescricao);
-                        
-                        
                         return (
                             <div className="col-md-3" key={info.userNome}>
                             
                                 <div className="card mt-3">
-                                    <div className="card-title text-center">
-                                        <b>Nome</b><br/> 
-                                        <span className="badge badge-primary"><h6>{info.userNome}</h6> </span><br/>
+                                    <div className="card-header text-center text-white bg-primary" style={{paddingTop: '20px'}}>
+                                        
+                                        <h6>{info.userNome}</h6> 
                                         
                                     </div>
                                     <div className="card-body">
-                                        Email <br/>
-                                        {info.userEmail}<br/>
-                                        Descrição <br/>
-                                        {info.userDescricao} <br/>
-                                        <AnaliseTexto info={newInfos}/>
+                                        <strong style={{fontSize: '14px'}}>Email</strong><br/>
+                                        {info.userEmail}<br/><br/>
+                                        <strong style={{fontSize: '14px'}}>Descrição</strong> <br/>
+                                        {info.userDescricao} <br/><br/>
+                                        <AnaliseTexto descricao={info.userDescricao}/>
                                     </div>
                                 </div>
                             </div>
