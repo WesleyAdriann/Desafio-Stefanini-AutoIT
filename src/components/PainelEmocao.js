d2ab1af2e3da8e83a2b1bb3ff9ba08786622d67c
Wesley Souza <wesleyadriann@gmail.com>
2019-03-20 22:38:36 -0300

    Faz o map com novas informações

--- src/components/PainelEmocao.js
+++ src/components/PainelEmocao.js
@@ -4,14 +4,16 @@ import axios from 'axios';
 class PainelEmocao extends Component {
     constructor(props) {
         super(props);
-
         this.data = props.data;
-        // this.handleDescricao = this.handleDescricao.bind(this);
+        this.state = {
+            idioma : [],
+        }
+        this.handleDescricao = this.handleDescricao.bind(this);
     }
 
     async handleDescricao (descricao) {
         const key = "9a1b21c339c1401e8c486ca264d333f5";
-        console.log("handle");
+        
         let responseIdioma = await axios({
             method: 'post',
             url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/languages',
@@ -65,37 +67,46 @@ class PainelEmocao extends Component {
         })
         responseSentimento = responseSentimento.data.documents[0].score;
         
-        console.log(responseIdioma, responseFrases, responseSentimento);
-        return [responseIdioma, responseFrases, responseSentimento];
+        const completeInfos = [responseIdioma, responseFrases, responseSentimento];
+        console.log(completeInfos);
+        return (completeInfos);
+            
     }
 
+
+
     render () {
+        
         return (
             <div className="row">
-                {this.data.map( info => {
-                    let infosDescricao = this.handleDescricao(info.userDescricao);
-                    
-                    console.log(infosDescricao);
-                    return (
-                        <div className="col-md-3" key={info.userNome}>
-                            <div className="card mt-3">
-                                <div className="card-title text-center">
-                                    <b>Nome</b><br/> 
-                                    <span className="badge badge-primary"><h6>{info.userNome}</h6> </span><br/>
-                                    
-                                </div>
-                                <div className="card-body">
-                                    Email <br/>
-                                    {info.userEmail}<br/>
-                                    Descrição <br/>
-                                    {info.userDescricao} <br/>
-                                   {}
-                                    
+                {
+                    this.data.map(info => { 
+                        let newInfos = this.handleDescricao(info.userDescricao);
+                        sleep(2000);
+                        console.log(newInfos);
+                        return (
+                            <div className="col-md-3" key={info.userNome}>
+                            
+                                <div className="card mt-3">
+                                    <div className="card-title text-center">
+                                        <b>Nome</b><br/> 
+                                        <span className="badge badge-primary"><h6>{info.userNome}</h6> </span><br/>
+                                        
+                                    </div>
+                                    <div className="card-body">
+                                        Email <br/>
+                                        {info.userEmail}<br/>
+                                        Descrição <br/>
+                                        {info.userDescricao} <br/>
+                                        {newInfos[0]}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                        
+                    })
+                }
+                
             </div>
         );
     }
