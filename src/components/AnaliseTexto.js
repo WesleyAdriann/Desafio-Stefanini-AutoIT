fb08ee3689fcd861d933cce9e2dc1d6b1964aa57
Wesley Souza <wesleyadriann@gmail.com>
2019-03-21 01:20:39 -0300

    Finaliza o component de analise de texto

--- src/components/AnaliseTexto.js
+++ src/components/AnaliseTexto.js
@@ -5,7 +5,7 @@ class AnaliseTexto extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            palavrasChave : '',
+            palavrasChave : [],
             lingua: '',
             sentimento: '',
             flag: false,
@@ -21,7 +21,7 @@ class AnaliseTexto extends Component {
     }
 
     handleLingua = descricao => {
-        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        const key = "b7857a3c744e4b589098202a253ec8f5";
         axios({
             method: 'post',
             url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/languages',
@@ -39,7 +39,7 @@ class AnaliseTexto extends Component {
 
         }).then (response => {
             this.setState({
-                lingua : response.data.documents[0].detectedLanguages,
+                lingua : response.data.documents[0].detectedLanguages[0],
                 flag: true,
             })
         })
@@ -47,7 +47,7 @@ class AnaliseTexto extends Component {
     }
 
     handleSentimento = descricao => {
-        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        const key = "b7857a3c744e4b589098202a253ec8f5";
         axios({
             method: 'post',
             url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
@@ -64,7 +64,7 @@ class AnaliseTexto extends Component {
                 ]
             }
         }).then (response => {
-            console.log(response);
+            
             this.setState({
                 sentimento : response.data.documents[0].score,
                 flag: true,
@@ -73,7 +73,7 @@ class AnaliseTexto extends Component {
     }
 
     handlePalavras = descricao => {
-        const key = "9a1b21c339c1401e8c486ca264d333f5";
+        const key = "b7857a3c744e4b589098202a253ec8f5";
         axios({
             method: 'post',
             url: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
@@ -106,9 +106,28 @@ class AnaliseTexto extends Component {
         window.setTimeout(this.reRender, 2000);
         return (
             <div>
+                <strong style={{fontSize: '14px'}}>Lingua</strong> <br/>
+                
+                
+                {this.state.lingua.name} - { this.state.lingua.iso6391Name}<br/><br/>
+                
+                
+                
+                <strong style={{fontSize: '14px'}}>Palavras Chaves</strong> <br/>
+                {Object.entries(this.state.palavrasChave).map(([i, value]) => {
+                    
+                    return (
+                        <span key={i} className="badge badge-primary ml-1">
+                        {value}
+                        
+                        </span> 
+                    )
+                }) 
+                }
+                 <br/><br/>
                 <strong style={{fontSize: '14px'}}>Sentimento</strong> <br/>
                 <div className="progress">
-                <div class="progress-bar" role="progressbar" style={{width: `${this.state.sentimento*100}%`}}>{parseInt(this.state.sentimento*100)}%</div>
+                <div className="progress-bar" role="progressbar" style={{width: `${this.state.sentimento*100}%`}}>{parseInt(this.state.sentimento*100)}%</div>
                 </div>
             </div>
         )
